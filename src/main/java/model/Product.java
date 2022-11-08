d8069e54ffe5bd23d8a62b113cf34ffdc20d6e88
BekhterNikolay <n.v.bekhter@mail.ru>
2022-11-08 20:40:48 +0300

    lesson_1

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+
+    public String id;
+    private String title;
+    private int cost;
+    private List<Product> children;
+
+}
